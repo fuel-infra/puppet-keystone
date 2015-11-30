cc00ddb8ea3a6a5daeaa6b9b37bc110f0cc5c008
Michael Polenchuk <mpolenchuk@mirantis.com>
2015-11-30 20:25:26 +0300

    Make request by service token if got unauth error
    In case then we have openrc before evaluating keystone admin role
    the service token auth method should be used instead
    of user/password one.
    
    Change-Id: I7b0d87487cb2f4715b6f1255d1e3833c3cd3ea32
    Partial-bug: #1511693

--- spec/unit/provider/keystone_spec.rb
+++ spec/unit/provider/keystone_spec.rb
@@ -123,6 +123,7 @@ describe Puppet::Provider::Keystone do
       home = ENV['HOME']
       ENV.clear
       File.expects(:exists?).with("#{home}/openrc").returns(false)
+      File.expects(:exists?).with('/root/openrc').returns(false)
       File.expects(:exists?).with("/etc/keystone/keystone.conf").returns(false)
       expect(klass.get_auth_url).to be_nil
     end
